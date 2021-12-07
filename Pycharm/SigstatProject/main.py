52b84c547b06c1bc5c842045316fce0e1214da11
Levente Nagy <levinagy44@gmail.com>
2021-12-07 10:01:37 +0100

    cuda for sure

--- Pycharm/SigstatProject/main.py
+++ Pycharm/SigstatProject/main.py
@@ -3,49 +3,47 @@ import model as SiameseModel
 import loss as CL
 import img_read
 import train
-import numpy as np
-import random
 
-
-#eszkoz letrehozasa, init
+# eszkoz letrehozasa, init
 torch.manual_seed(444)
 torch.cuda.manual_seed_all(444)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(torch.cuda.is_available())
-#model attoltese
+# model attoltese
 model = SiameseModel.SigSiameseNet().to(device)
 
 optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-4, eps=1e-8, weight_decay=5e-4, momentum=0.9)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 5, 0.1)
 
+loss = CL.ContrastiveLoss(10e-4, 0.75, 1).to(device)
 epoch_num = 20
 
-#ez az altalunk irt dataloader, nem beepitettet hasznalok
+# ez az altalunk irt dataloader, nem beepitettet hasznalok
 DataLoader = img_read.DataLoader()
 
 DataLoader.load()
 
-#Összes valos eredeti es hamis alairas
+# Összes valos eredeti es hamis alairas
 genuine_images = DataLoader.genuine_images
 forged_images = DataLoader.forged_images
 
 # TODO test train split
 train_pairs, test_pairs = img_read.createPairs(genuine_images, forged_images)
 model.train()
-#print(model)
+# print(model)
 
 # teszt tomb megnezni hogy jol mukodik-e a model
 
 
 # futtatas
+
 for epoch in range(epoch_num):
     print('Epoch {}/{}'.format(epoch, epoch_num))
     print('Training', '-' * 20)
-    train.train(model, optimizer, device, train_pairs)
+    train.train(model, optimizer, device, train_pairs, loss)
     print('Evaluating', '-' * 20)
-    train.eval(model, device, test_pairs)
+    lost, acc = train.eval(model, device, test_pairs, loss)
     scheduler.step()
-
 
     to_save = {
         'model': model.state_dict(),
@@ -54,6 +52,6 @@ for epoch in range(epoch_num):
     }
 
     print('Saving checkpoint..')
-   # torch.save(to_save, 'checkpoints/epoch_{}_loss_{:.3f}_acc_{:.3f}.pt'.format(epoch, loss, acc))
+    torch.save(to_save, 'checkpoints/epoch_{}_loss_{:.3f}_acc_{:.3f}.pt'.format(epoch, lost, acc))
 
 print('Done')
--- Pycharm/SigstatProject/train.py
+++ Pycharm/SigstatProject/train.py
@@ -1,46 +1,42 @@
 import torch
 import loss as CL
 
-#train fuggveny batchekkel meg nem foglalkozik, sem a hiba kiertekelesevel, eloszor csak atakartam rajta futtatni par adatot
 
-def train(model, optimizer, device, data):
-    #TODO nem ertem hogy mukodik a to device es nem tudom, hogy ha egy masik file-ben atrakom a valtozot
+# train fuggveny batchekkel meg nem foglalkozik, sem a hiba kiertekelesevel, eloszor csak atakartam rajta futtatni par adatot
+
+def train(model, optimizer, device, data, loss):
+    # TODO nem ertem hogy mukodik a to device es nem tudom, hogy ha egy masik file-ben atrakom a valtozot
     # es atadom parameterkent megtartja-e a tulajdonsagat
 
-    loss = CL.ContrastiveLoss(10e-4, 0.75, 1).to(device)
+    print(loss)
 
     model.train()
-    #TODO batch implementalasa
-
+    # TODO batch implementalasa
 
     # az osszes adatot atkuljuk a halon, (kep1, kep1,y)
     for pair in data:
-        #TODO valoszinuleg ez nem mukodik itt jol
+        # TODO valoszinuleg ez nem mukodik itt jol
         s1 = torch.tensor(pair[0]).float().to(device)
         s2 = torch.tensor(pair[1]).float().to(device)
 
-
-        #Itt az y erteke teljesen megvaltozik valmire, nem tudom hogy a to(Device) vagy a ShortTensor rontja el
+        # Itt az y erteke teljesen megvaltozik valmire, nem tudom hogy a to(Device) vagy a ShortTensor rontja el
         y = torch.tensor(pair[2]).to(device)
         print(y)
         print(pair[2])
         print(s1.shape)
 
-
         optimizer.zero_grad()
         s1, s2 = model(s1, s2)
-        result = loss(s1, s2, y) #itt dobja a hibat
-        #Boolean value of Tensor with more than one value is ambiguous
+        result = loss(s1, s2, y)  # itt dobja a hibat
+        # Boolean value of Tensor with more than one value is ambiguous
 
         result.backward()
         optimizer.step()
 
 
-#eval fv, idaig el se jutunk
+# eval fv, idaig el se jutunk
 @torch.no_grad()
-def eval(model, device, data):
-    loss = CL.ContrastiveLoss(10e-4, 0.75, 1).to(device)
-
+def eval(model, device, data, loss):
     model.eval()
 
     for pair in data:
@@ -51,5 +47,5 @@ def eval(model, device, data):
         s1, s2 = model(s1, s2)
         result = loss(s1, s2, y)
 
-    #TODO calculate accuracy
+    # TODO calculate accuracy
     return
